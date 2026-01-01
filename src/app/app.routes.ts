0a749ee5c4ccf07c87c37d9c867052f86f726c25
Renzo Pucci <renzo@MacBook-Air-di-Renzo-2.local>
2026-01-01 00:40:56 +0000

    Configure routes for all pages

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -3,7 +3,20 @@ import { Routes } from '@angular/router';
 export const routes: Routes = [
   {
     path: 'home',
-    loadComponent: () => import('./home/home.page').then((m) => m.HomePage),
+    loadComponent: () => import('./home/home.page').then(m => m.HomePage),
+  },
+  {
+    path: 'recipe-details/:id',
+    loadComponent: () =>
+      import('./recipe-details/recipe-details.page').then(m => m.RecipeDetailsPage),
+  },
+  {
+    path: 'favourite',
+    loadComponent: () => import('./favourite/favourite.page').then(m => m.FavouritePage),
+  },
+  {
+    path: 'settings',
+    loadComponent: () => import('./settings/settings.page').then(m => m.SettingsPage),
   },
   {
     path: '',
@@ -11,15 +24,7 @@ export const routes: Routes = [
     pathMatch: 'full',
   },
   {
-    path: 'recipe-details',
-    loadComponent: () => import('./recipe-details/recipe-details.page').then( m => m.RecipeDetailsPage)
-  },
-  {
-    path: 'favourite',
-    loadComponent: () => import('./favourite/favourite.page').then( m => m.FavouritePage)
-  },
-  {
-    path: 'settings',
-    loadComponent: () => import('./settings/settings.page').then( m => m.SettingsPage)
+    path: '**',
+    redirectTo: 'home',
   },
 ];
