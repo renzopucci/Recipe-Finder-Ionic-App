c1b694b50d77d06afeb14322d3be46f920fe3b88
Renzo Pucci <renzopucci@hotmail.it>
2026-01-01 01:24:55 +0000

    Fix routerLink by importing RouterLink in HomePage

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,12 +1,53 @@
+import { RouterLink } from '@angular/router';
 import { Component } from '@angular/core';
-import { IonHeader, IonToolbar, IonTitle, IonContent } from '@ionic/angular/standalone';
+import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
+import { IonicModule } from '@ionic/angular';
+import { RecipeService, RecipeSearchResult } from '../services/recipe.service';
 
 @Component({
+  
   selector: 'app-home',
-  templateUrl: 'home.page.html',
-  styleUrls: ['home.page.scss'],
-  imports: [IonHeader, IonToolbar, IonTitle, IonContent],
+  templateUrl: './home.page.html',
+  styleUrls: ['./home.page.scss'],
+  standalone: true,
+  imports: [IonicModule, CommonModule, FormsModule, RouterLink],
+  
 })
 export class HomePage {
-  constructor() {}
+  ingredients = '';
+  recipes: RecipeSearchResult[] = [];
+  isLoading = false;
+  errorMessage = '';
+
+  constructor(private recipeService: RecipeService) {}
+
+  search() {
+    const q = this.ingredients.trim();
+
+    this.errorMessage = '';
+    this.recipes = [];
+
+    if (!q) {
+      this.errorMessage = 'Please enter at least one ingredient.';
+      return;
+    }
+
+    this.isLoading = true;
+
+    this.recipeService.searchRecipes(q).subscribe({
+      next: (results) => {
+        this.recipes = results ?? [];
+        if (this.recipes.length === 0) {
+          this.errorMessage = 'No recipes found. Try different ingredients.';
+        }
+        this.isLoading = false;
+      },
+      error: (err) => {
+        console.error(err);
+        this.errorMessage = 'Error calling Spoonacular API.';
+        this.isLoading = false;
+      },
+    });
+  }
 }
